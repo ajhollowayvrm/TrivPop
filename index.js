34bd60f7c621a4d84d525f0e95352c5ba5f9ba02
AJ <ajhollowayvrm@gmail.com>
2018-01-19 00:17:39 -0700

    Built initial landing page. Build initial buttons.

---
+++ index.js
@@ -0,0 +1,14 @@
+const express = require('express')
+const path = require('path')
+const app = express()
+
+
+//Serve up public files.
+app.use(express.static(path.join(__dirname,'public'))); 
+
+
+app.get('/', function (req, res) {
+    res.sendFile(path.join(__dirname + '/home.html'));
+})
+
+app.listen(3000, () => console.log('Listening on Port 3000'))
