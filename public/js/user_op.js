2d44c33203d42217c4464f1fdfd1c7d0b0ff6bfa
AJ <ajhollowayvrm@gmail.com>
2018-02-09 01:03:45 -0700

    Added some transformations; Started movement JS

--- public/js/user_op.js
+++ public/js/user_op.js
@@ -1,13 +1,43 @@
 $(document).ready(function() {
-    $('#Log_In_Btn').click(function() {
 
+    //Set up button functions.
+    $('#Log_In_Btn').click(function() {
+        login();
     });
     $('#Sign_Up_Btn').click(function() {
-
+        signUp();
     });
 })
 
-
 function login() {
-    
+    moveToTile('login');
 }
+
+function signUp() {
+
+}
+
+function moveToTile(tile_name) {
+    $(getCurrentTile()).fadeOut().attr({'tileview':'no'});
+    $("[tile='" + tile_name + "']").fadeIn().attr({'tileview':'yes'})
+}
+
+function getCurrentTile() {
+    $('#main-container').each(function() {
+        if($(this).tileview() == 'yes')
+            return this;
+    })
+}
+
+
+//----------------------Utilities--------------------//
+
+//Custom jQuery function to get the tile and tileview values.
+jQuery.fn.extend({
+    tile: function() {
+        return this.attr('tile');
+    },
+    tileview: function() {
+        return this.attr('tileview');
+    }
+})
